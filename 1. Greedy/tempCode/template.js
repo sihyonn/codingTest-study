d70bd075cdfc682c5bad7a61d12636c7b1cc070e
sihyonn <sihyeon982@naver.com>
2023-11-27 11:28:13 +0900

    feat: vscode용, 백준제출용 템플릿

---
+++ 1. Greedy/tempCode/template.js
@@ -0,0 +1,17 @@
+/**
+ * vscode 풀이용
+ */
+//입력정제
+const fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+
+//문제풀이
+function solution(test1, test2) {}
+
+solution(test1, test2);
+
+/**
+ * 백준 제출용
+ */
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
