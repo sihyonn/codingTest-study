e888da9c2fc8709aa9bd85d0b6f56efff85c80f4
mun-jihye <m84422583@gmail.com>
2024-01-15 21:37:50 +0900

    chore: 파일 생성

---
+++ 2. Sorting/boj_1377.js
@@ -0,0 +1,14 @@
+/**
+ * 1377 [버블 소트]
+ * 2024.01.15.월요일
+ * https://www.acmicpc.net/problem/1377
+ *
+ * 입력 : 500,000보다 작거나 같은 자연수 / 째 줄부터 N개의 줄에 A[1]부터 A[N]까지 하나씩 주어짐
+ * 출력 :  정답
+ * point : 버블 정렬을 js로 구현하면 되나?
+ */
+let input = require("fs")
+  .readFileSync("2. Sorting/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
