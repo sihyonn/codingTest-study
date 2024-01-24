87439c749464504e9f8f6f2ab9ac3519b4adb127
mun-jihye <m84422583@gmail.com>
2024-01-24 21:38:31 +0900

    feat: solution boj_2805

--- 4. BS/boj_2805.js
+++ 4. BS/boj_2805.js
@@ -8,7 +8,38 @@
  * point :
  */
 const input = require("fs")
-  .readFileSync("4. BS/input.txt")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "4. BS/input.txt")
   .toString()
   .trim()
   .split("\n");
+const [N, M] = input.shift().split(" ");
+const tree = input[0]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+function solve(arr, target) {
+  let start = 0;
+  let end = arr[arr.length - 1];
+  let answer = Number.MIN_SAFE_INTEGER;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let sum = 0;
+    for (let x of arr) {
+      if (x > mid) sum += x - mid;
+    }
+
+    if (sum >= target) {
+      if (mid > answer) answer = mid;
+      // 최댓값 계속 구해주기.
+      // 이 부분을 제외하고는 일반적인 이분탐색 코드와 똑같다.
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solve(tree, M));
