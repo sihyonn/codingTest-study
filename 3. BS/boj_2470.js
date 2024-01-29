6c4eda6f43bb3f516e0cf85751d212feb228d5a3
Hooni07 <gkxogns21@gmail.com>
2024-01-29 10:05:38 +0900

    feat: config week4 problems

---
+++ 3. BS/boj_2470.js
@@ -0,0 +1,41 @@
+/*
+ * 백준 2470번 [두 용액] -> 공통 문제
+ * 2024.01.23.화
+ */
+
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input_boj2110.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const input_check1 = input[0].split(' ');
+const input_C = +input_check1[1];
+  
+let positions = [];
+for (let i=1; i < input.length; i++) {
+  let temp = +input[i];
+  positions.push(temp);
+}
+
+positions.sort((a, b) => a - b);
+
+let start = 1;
+let end = positions[positions.length - 1];
+
+while (start <= end) {
+  const mid = Math.floor((start + end) / 2);
+
+  let count = 1;
+  let prev = positions[0];
+  for (const cur of positions) {
+    if (cur - prev < mid) continue;
+    prev = cur;
+    count += 1;
+  }
+
+  if (count < input_C) end = mid - 1;
+  else start = mid + 1;
+}
+
+console.log(end);
