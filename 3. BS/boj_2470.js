eacd7f5db5525430f8b4af960e4839ba396f3c70
Hooni07 <gkxogns21@gmail.com>
2024-01-30 12:06:17 +0900

    feat: solved boj_2470

--- 3. BS/boj_2470.js
+++ 3. BS/boj_2470.js
@@ -9,6 +9,47 @@ const input = require('fs')
   .split('\n');
 
 const [N] = input[0].split(' ').map(Number);
-const arr = input[1].split(' ').map(Number).sort((a, b) => a-b);
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b);
 
-console.log(N, arr);
+let left = arr[0];
+let right = arr[N - 1];
+let result_left = 0, result_right = 0;
+
+if (right <= 0){
+  result_left = arr[N-2];
+  result_right = arr[N-1];
+}
+else if (left >= 0) {
+  result_left = arr[0];
+  result_right = arr[1];
+}
+else {
+  const mid = 0;
+  let close = Number.MAX_SAFE_INTEGER;
+  let left_index = 0, right_index = N - 1;
+
+  while (left < right) {
+    if (left + right === mid){
+      result_left = left;
+      result_right = right;
+      break;
+    } else if (left + right > mid) {  // left, right의 합이 양수인 경우
+      if (Math.abs(left + right) < close){
+        close = Math.abs(left + right);
+        result_left = left;
+        result_right = right;
+      }
+      right = arr[right_index--];
+    } else { // left, right의 합이 음수인 경우
+      if (Math.abs(left + right) < close){
+        close = Math.abs(left + right);
+        result_left = left;
+        result_right = right;
+      }
+      left = arr[left_index++];
+    }
+  }
+}
+
+
+console.log(result_left, result_right);
