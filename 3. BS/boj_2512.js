3baf267b1088f9a0d791247589bb3c472f3b610e
Hooni07 <gkxogns21@gmail.com>
2024-02-02 09:50:09 +0900

    feat: config boj_2512 input

--- 3. BS/boj_2512.js
+++ 3. BS/boj_2512.js
@@ -1,43 +1,16 @@
 /*
  * 백준 2512번 [예산] -> 공통 문제
- * 2024.01.25.목
+ * 2024.02.02.금
  */
 
 const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input_boj2805.txt')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input_boj2512.txt')
   .toString()
-  .trim()
   .split('\n');
 
 // setting input
-const input_check = input[0].split(' ');
-const N = +input_check[0];
-const M = +input_check[1];
+const N = +input[0];
+const arr = input[1].split(' ').map(Number);
+const M = +input[2];
 
-const trees = [];
-const input_check2 = input[1].split(' ');
-for (let i=0; i < input_check2.length; i++) {
-  let temp = +input_check2[i];
-  trees.push(temp);
-}
-
-trees.sort((a, b) => a - b);
-
-let start = 1;
-let end = trees[N-1];
-while (start <= end) {
-  let cutting_length = 0;
-  let mid = Math.floor((start + end) / 2);
-
-  for (const cur of trees) {
-    if (cur - mid < 0) continue;
-    cutting_length += cur - mid;
-  }
-  if (cutting_length < M) {
-    end = mid - 1;
-  } else {
-    start = mid + 1;
-  }
-}
-
-console.log(end);
+console.log(N, arr, M);
