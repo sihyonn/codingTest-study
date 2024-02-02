24b80ec35cabc68e68eacf10cac1ad9f27bd1c0b
Hooni07 <gkxogns21@gmail.com>
2024-02-02 11:23:14 +0900

    feat: solved boj_2512

--- 3. BS/boj_2512.js
+++ 3. BS/boj_2512.js
@@ -10,7 +10,34 @@ const input = require('fs')
 
 // setting input
 const N = +input[0];
-const arr = input[1].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b);
 const M = +input[2];
 
-console.log(N, arr, M);
+let left = 1;
+let right = arr[N - 1];
+
+let maxSum = 0;
+while (left <= right) {
+  const mid = Math.floor((left + right) / 2);
+  let temp_sum = 0;
+  
+  for (const local of arr) {
+    if (local >= mid){
+      temp_sum += mid;
+    } else {
+      temp_sum += local;
+    }
+  }
+
+  if (temp_sum <= M) {
+    if (temp_sum > maxSum) {
+      maxSum = temp_sum;
+      left = mid + 1;
+    }
+  }
+  else {
+    right = mid - 1;
+  }
+}
+
+console.log(right);
